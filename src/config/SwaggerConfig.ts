4dbb5ed1c699eccf8d4538bdeed3ebfd9a8a1862
CristianoMends <mendescristiano012@gmail.com>
2025-01-06 08:06:48 -0300

    update swagger

--- src/config/SwaggerConfig.ts
+++ src/config/SwaggerConfig.ts
@@ -2,35 +2,26 @@ import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export class SwaggerConfig {
-
-
   static setup(app: INestApplication): void {
     const config = new DocumentBuilder()
       .setTitle('Portifolio API')
-      .setDescription('Documentação para a api do portifolio')
+      .setDescription('Documentação para a API do portfólio')
       .setVersion('1.0')
       .addBearerAuth()
       .addServer('https://portifolioapi.vercel.app')
       .build();
 
+    const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.min.css';
+    const JS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-bundle.min.js';
+
     const document = SwaggerModule.createDocument(app, config);
 
     SwaggerModule.setup('api', app, document, {
       swaggerOptions: {
-        persistAuthorization: true,
+        persistAuthorization: true, 
       },
-      customJs: [
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-bundle.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-standalone-preset.min.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-standalone-preset.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-bundle.js'
-      ],
-      customCssUrl: [
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-standalone-preset.min.css',
-        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.css',
-      ],
+      customCssUrl: CSS_URL, 
+      customJs: JS_URL, 
       customSiteTitle: 'Portifolio API - Documentação',
     });
   }
