6208cb3853554c23debf92b979dd015bd3ddae78
CristianoMends <mendescristiano012@gmail.com>
2025-01-06 07:57:45 -0300

    update swagger

--- src/config/SwaggerConfig.ts
+++ src/config/SwaggerConfig.ts
@@ -13,17 +13,21 @@ export class SwaggerConfig {
       .addServer('https://portifolioapi.vercel.app')
       .build();
 
-    const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.css";
-    const SWAGGER_BUDLE = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-bundle.js";
-
     const document = SwaggerModule.createDocument(app, config);
 
     SwaggerModule.setup('api', app, document, {
       swaggerOptions: {
         persistAuthorization: true,
       },
-      customCssUrl: CSS_URL,
-      customJs: SWAGGER_BUDLE,
+      customJs: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-bundle.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-standalone-preset.min.js',
+      ],
+      customCssUrl: [
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-standalone-preset.min.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.css',
+      ],
       customSiteTitle: 'Portifolio API - Documentação',
     });
   }
