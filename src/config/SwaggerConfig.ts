238da15769068edadea63d19b82ab94c601e0961
CristianoMends <mendescristiano012@gmail.com>
2025-01-06 08:02:18 -0300

    update swagger

--- src/config/SwaggerConfig.ts
+++ src/config/SwaggerConfig.ts
@@ -22,6 +22,9 @@ export class SwaggerConfig {
       customJs: [
         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-bundle.min.js',
         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-standalone-preset.min.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.css',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-standalone-preset.js',
+        'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui-bundle.js'
       ],
       customCssUrl: [
         'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/5.18.2/swagger-ui.min.css',
