5695db602eb550ab738b14da8a8b02665f1d4215
CristianoMends <mendescristiano012@gmail.com>
2025-01-07 11:09:34 -0300

    add axios

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Aplicação') 
 @Controller()
 export class AppController {
-
+  
+  @Get()
+  @ApiOperation({ summary: 'Retorna uma mensagem de Olá Mundo' })
+  @ApiResponse({
+    status: 200,
+    description: 'Mensagem de sucesso retornada.',
+    type: String,
+  })
+  public getHello() {
+    return "Olá mundo!";
+  }
 }
